1b606e9f6ba1e20e17bfcc24f7b03cab68ede21d
vikas <vkbookworm@gmail.com>
2019-08-13 23:19:11 +0530

    fix: fixed the details page navigation click listener

--- Flurry Notes/controller/DetailsViewController.swift
+++ Flurry Notes/controller/DetailsViewController.swift
@@ -15,9 +15,24 @@ class DetailsViewController: UIViewController {
     let navigationBar : MDCNavigationBar = {
         let appBar = MDCNavigationBar()
         appBar.backgroundColor = ApplicationScheme.shared.colorScheme.primaryColor
-        let backImg = UIImage(named: "back")
-        let backButton = UIBarButtonItem(image: backImg!,style: .plain,target: self, action: #selector(backPressed(sender:)))
-        appBar.backItem = backButton
+        
+        //back button
+        let menuItemImage = UIImage(named: "back")
+        let templatedMenuItemImage = menuItemImage?.withRenderingMode(.alwaysOriginal)
+        let menuItem = UIBarButtonItem(image: templatedMenuItemImage,
+                                       style: .plain,
+                                       target: self,
+                                       action: #selector(backPressed(sender:)))
+        appBar.leftBarButtonItem = menuItem
+        
+        //more button
+        let menuItemImageMore = UIImage(named: "more")
+        let templatedMenuItemMore = menuItemImageMore?.withRenderingMode(.alwaysOriginal)
+        let menuItemMore = UIBarButtonItem(image: templatedMenuItemMore,
+                                       style: .plain,
+                                       target: self,
+                                       action: #selector(backPressed(sender:)))
+        appBar.rightBarButtonItem = menuItemMore
         return appBar
     }()
     
@@ -27,6 +42,17 @@ class DetailsViewController: UIViewController {
         scrollView.backgroundColor = ApplicationScheme.shared.colorScheme.primaryColor
         scrollView.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
         return scrollView
+    }()
+    
+    let imageCategoryView: UIImageView = {
+        let img = UIImage(named: "document")?.withInset(UIEdgeInsets(top: 40, left: 40, bottom: 40, right: 40))
+        let category = UIImageView(image: img)
+        category.autoresizingMask = [.flexibleRightMargin, .flexibleLeftMargin, .flexibleBottomMargin]
+        category.contentMode = UIView.ContentMode.scaleAspectFit
+        category.layer.backgroundColor = ApplicationScheme.shared.colorScheme.backgroundColor.cgColor
+        category.clipsToBounds = true
+        category.layer.cornerRadius = 30
+        return category
     }()
 
     override func viewDidLoad() {
@@ -46,27 +72,46 @@ class DetailsViewController: UIViewController {
             make.top.equalTo(self.view.safeAreaLayoutGuide)
             make.bottom.equalToSuperview()
         }
-        self.scrollView.addSubview(navigationBar)
+        //navigation bar
+        self.view.addSubview(navigationBar)
         self.navigationBar.snp.makeConstraints{ make -> Void in
             make.top.equalTo(scrollView.snp.top)
             make.left.equalTo(scrollView.snp.left)
             make.right.equalTo(scrollView.snp.right)
         }
+        
+        //category image view
+        self.view.addSubview(imageCategoryView)
+        self.imageCategoryView.snp.makeConstraints{make->Void in
+            make.top.equalTo(self.navigationBar.snp.bottom).offset(70)
+            make.leftMargin.equalTo(40)
+            make.height.equalTo(60)
+            make.width.equalTo(60)
+        }
     }
     
     @objc func backPressed(sender:Any?){
-        print("back pressed")
+        self.dismiss(animated: true)
+        print("back pressed details")
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+/**
+extension functions
+ **/
+extension UIImage {
+
+    func withInset(_ insets: UIEdgeInsets) -> UIImage? {
+        let cgSize = CGSize(width: self.size.width + insets.left * self.scale + insets.right * self.scale,
+                            height: self.size.height + insets.top * self.scale + insets.bottom * self.scale)
+        
+        UIGraphicsBeginImageContextWithOptions(cgSize, false, self.scale)
+        defer { UIGraphicsEndImageContext() }
+        
+        let origin = CGPoint(x: insets.left * self.scale, y: insets.top * self.scale)
+        self.draw(at: origin)
+        
+        return UIGraphicsGetImageFromCurrentImageContext()?.withRenderingMode(self.renderingMode)
     }
-    */
-
 }
