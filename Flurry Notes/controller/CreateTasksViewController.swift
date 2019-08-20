68776e3119e9e0596cdc103a461e96cfef2159c8
vikas <vkbookworm@gmail.com>
2019-08-21 00:09:13 +0530

    feat: added the close button for create tasks controller

--- Flurry Notes/controller/CreateTasksViewController.swift
+++ Flurry Notes/controller/CreateTasksViewController.swift
@@ -7,24 +7,48 @@
 //
 
 import UIKit
+import MaterialComponents
+import SnapKit
 
 class CreateTasksViewController: UIViewController {
 
+    
+    //navigation view
+    let navigationBar : MDCNavigationBar = {
+        let appBar = MDCNavigationBar()
+        
+           //close button
+        let menuItemImageMore = UIImage(named: "close")
+        let templatedMenuItemMore = menuItemImageMore?.withRenderingMode(.alwaysOriginal)
+        let menuItemMore = UIBarButtonItem(image: templatedMenuItemMore,
+                                           style: .plain,
+                                           target: self,
+                                           action: #selector(backPressed(sender:)))
+        appBar.rightBarButtonItem = menuItemMore
+        appBar.title = "New Task"
+        return appBar
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        assembleViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func assembleViews(){
+        self.view.addSubview(navigationBar)
+        self.navigationBar.snp.makeConstraints{ make -> Void in
+            make.top.equalTo(self.view.safeAreaLayoutGuide)
+            make.left.equalTo(self.view.safeAreaLayoutGuide)
+            make.right.equalTo(self.view.safeAreaLayoutGuide)
+        }
     }
-    */
+
+    @objc func backPressed(sender:Any?){
+        self.dismiss(animated: true)
+        print("back pressed details")
+    }
 
 }
